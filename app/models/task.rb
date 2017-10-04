d6e67d365483aacdb2d07fafe2d79961a69a9fa0
hiroki-tajima <hiroki-tajima@P708.local>
2017-10-04 17:06:48 +0900

    Validation対応

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,11 @@
 class Task < ApplicationRecord
+  #Validation
+  #title  length_100以内 必須
+  validates :title, length:{ maximum: 100 }, presence: true
+
+  #description  length_500以内
+  validates :description, length:{ maximum: 500 }
+
+  #importance  範囲 1~10
+  validates :importance, inclusion:{ in: 1..10 }
 end

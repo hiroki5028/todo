ef4975ee275fa100f4daad710705b491dc5a941e
hiroki-tajima <hiroki-tajima@P708.local>
2017-10-16 14:27:22 +0900

    usersテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013060241) do
+ActiveRecord::Schema.define(version: 20171016051505) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20171013060241) do
     t.datetime "updated_at", null: false
     t.boolean "completed", default: false, null: false
     t.integer "category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", default: "admin"
+    t.string "address"
+    t.string "password", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["address"], name: "index_users_on_address", unique: true
   end
 
 end
